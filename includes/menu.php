147365d311d4c349066d20cec532df7a39c85635
Steeven Toala <steevenpfb@gmail.com>
2025-04-29 17:35:20 -0500

    set up project structure

---
+++ includes/menu.php
@@ -0,0 +1,12 @@
+<header class="mb-auto">
+    <div>
+      <h3 class="float-md-start mb-0">AVIFEST</h3>
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Inicio</a>
+        <a class="nav-link fw-bold py-1 px-0" href="#">Detalles</a>
+        <a class="nav-link fw-bold py-1 px-0" href="#">Reservas</a>
+        <a class="nav-link fw-bold py-1 px-0" href="#">Contactanos</a>        
+      </nav>
+    </div>
+    
+  </header>
