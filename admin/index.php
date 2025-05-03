c7ba4e3f2c0a4ed226cbf34ae554356263445d73
Alexander <alexandertasinchano21@gmail.com>
2025-05-03 15:15:00 -0500

    se expone ruta de la api para obtener las solicitudes registradas en la base de datos

--- admin/index.php
+++ admin/index.php
@@ -82,5 +82,6 @@
 
   <!-- Scripts de Bootstrap -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="./js/admin_js.js"></script>
 </body>
 </html>
