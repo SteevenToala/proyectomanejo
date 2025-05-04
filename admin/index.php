63bd37a4e1fd4e55a4332ba81d8a3b902b6b9eb2
Alexander <alexandertasinchano21@gmail.com>
2025-05-04 10:39:06 -0500

    Se implementa logica de validacion de usuario logeado para ingresar ala pagina

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header("Location: ../login.php"); // Ajusta según tu estructura
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
