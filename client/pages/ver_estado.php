15e5fcb8fdb7138c68645eb298a8dcc0f2a4365b
Jaml888 <jaimadiel888@gmail.com>
2025-05-03 21:54:25 -0500

    Se hizo las interfaces de crear_solicitud.php y ver_estado.php

---
+++ client/pages/ver_estado.php
@@ -0,0 +1,24 @@
+<?php
+// Simular usuario logueado
+$usuario_id = 1;
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Ver Estado de Solicitud</title>
+</head>
+<body>
+    <h2>Estado de tu solicitud</h2>
+    <div id="estadoSolicitud">Cargando estado...</div>
+
+    <a href="../client/index.php">Volver</a>
+
+    <script>
+        // Pasar variable PHP a JS
+        const usuarioId = <?php echo json_encode($usuario_id); ?>;
+    </script>
+    <script src="../client/js/solicitudes.js"></script>
+</body>
+</html>
