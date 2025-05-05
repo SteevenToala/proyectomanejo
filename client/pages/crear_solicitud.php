e683f325f356bf438991827f1744e755e00ac9be
Kevin Peñafiel <alexpenafiel27@gmail.com>
2025-05-04 22:54:02 -0500

    Se agregaron mejoras a las interfaces del cliente

--- client/pages/crear_solicitud.php
+++ client/pages/crear_solicitud.php
@@ -10,14 +10,16 @@ $usuario_id = 1;
 <head>
     <meta charset="UTF-8">
     <title>Crear Solicitud</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../styles/crear_solicitud.css"> 
 </head>
-<body>
+<body class="background">
+  <div class="container">
     <h2>Solicitar participación en el evento</h2>
     <button id="btnSolicitar">Solicitar participación</button>
     <p id="mensaje"></p>
-
     <a href="../index.php">Volver</a>
-
+</div>
     <script>
         // Pasar variable PHP a JS
         const usuarioId = <?php echo json_encode($usuario_id); ?>;
